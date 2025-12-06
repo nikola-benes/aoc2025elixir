37ffdd9a52c4f5bc5b978deb5ac51e24cdf5c74b
Nikola Beneš <nikola.benes@gmail.com>
2025-12-06 14:06:50 +0100

    Day 6: Simplify part 2 slightly

--- 06_2.exs
+++ 06_2.exs
@@ -7,11 +7,9 @@ ops =
   |> Enum.map(fn "+" -> &Enum.sum/1; "*" -> &Enum.product/1 end)
 
 nums
-~> (String.replace_trailing("\n", "") |> to_charlist)
-|> Stream.zip_with(& &1)
-~> (to_string |> String.trim)
-|> Stream.chunk_by(& &1 == "")
-|> Stream.reject(& &1 == [""])
+~> to_charlist
+|> Stream.zip_with(& &1 |> to_string |> String.trim)
+|> stream_chunk_only(& &1 != "")
 ~> Stream.map(&String.to_integer/1)
 |> Stream.zip_with(ops, fn nums, op -> op.(nums) end)
 |> Enum.sum
--- aoc.ex
+++ aoc.ex
@@ -4,4 +4,8 @@ defmodule Aoc do
       unquote(e) |> Stream.map(fn x -> x |> unquote(f) end)
     end
   end
+
+  def stream_chunk_only(e, f) when is_function(f, 1) do
+    e |> Stream.chunk_by(f) |> Stream.filter(fn [x | _] -> f.(x) end)
+  end
 end
