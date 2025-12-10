ce17af7585d64c8602ad9c3182f27a7f57f108e9
Nikola Beneš <nikola.benes@gmail.com>
2025-12-10 16:56:11 +0100

    Day 10: Add solution of part 1

---
+++ 10_1.exs
@@ -0,0 +1,61 @@
+import Aoc
+import Bitwise
+
+defmodule Day10 do
+  def solve(line) do
+    {[goal | buttons], _} = line |> String.split |> Enum.split(-1)
+
+    goal =
+      goal
+      |> String.replace(["[", "]"], "")
+      |> to_charlist
+      |> Enum.reverse
+      |> bools_to_int(& &1 == ?#)
+
+    buttons =
+      buttons
+      |> Enum.map(fn b ->
+        b
+        |> String.replace(["(", ")"], "")
+        |> String.split(",")
+        |> Stream.map(& 1 <<< String.to_integer(&1))
+        |> Enum.sum
+      end)
+
+    bfs(goal, buttons, [0], MapSet.new, 0)
+  end
+
+  # Why the @#$% does the Bitwise module have bit operations for integers but
+  # not for bitstring?!
+
+  defp bools_to_int(bools) do
+    bools |> Enum.reduce(0, fn b, acc -> (acc <<< 1) + bool_to_int(b) end)
+  end
+
+  defp bools_to_int(bools, f) do
+    bools |> Stream.map(f) |> bools_to_int
+  end
+
+  defp bfs(goal, buttons, queue, seen, dist) do
+    dist = dist + 1
+
+    queue
+    |> Stream.flat_map(fn s -> buttons |> Stream.map(&bxor(&1, s)) end)
+    |> Enum.reduce_while({[], seen}, fn state, {next, seen} ->
+      cond do
+        state == goal -> {:halt, nil}
+        state in seen -> {:cont, {next, seen}}
+        true -> {:cont, {[state | next], MapSet.put(seen, state)}}
+      end
+    end)
+    |> then(fn
+      {next, seen} -> bfs(goal, buttons, next |> Enum.reverse, seen, dist)
+      nil -> dist
+    end)
+  end
+end
+
+IO.stream(:line)
+~> Day10.solve
+|> Enum.sum
+|> IO.puts
